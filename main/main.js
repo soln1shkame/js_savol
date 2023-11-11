89db3a237a15e499d88332ee57e8c6a03ba2013d
soln1shkame <soln1shkame08@gmail.com>
2023-11-11 19:31:00 +0500

    lorem

---
+++ main/main.js
@@ -0,0 +1,44 @@
+var Name = String(prompt('Birinchi muhabbatiz😄'))
+console.log(Name);
+
+var Surname = String(prompt('Birinchi kimga sovga olgansiz🎀'))
+console.log(Surname);
+
+var Current = String(prompt('Osha sovga nma edi🤔'))
+console.log(Current);
+
+var Delivery = String(prompt('Rasm chizib korganmisiz 🧑🏻‍🎨'))
+console.log(Delivery);
+
+var Vocation = String(prompt('Qanday rasm edi🎨'))
+console.log(Vocation);
+
+var Size = String(prompt('Qachon oshga boramiz ikkalamiz😹'))
+console.log(Size);
+
+var Numbern = Number(prompt('Ozinggizni 0dan 10gacha baxolang💯'))
+console.log(Number);
+
+var Occupation = String(prompt('Hobbyingiz😊'))
+console.log(Occupation);
+
+var Type = String(prompt('Qaysi davlatlarga bormoqchisiz ✈️'))
+console.log(Type);
+
+var info = String(prompt('Men haqimda fikriz🌝'))
+console.log(info);
+
+var All = [
+    'Birinchisi: '+' '+ Name +'\n' +
+    'Sovga: '+''+ Surname +'\n' +
+    'Nomi: '+''+ Current +'\n' +
+    'Rasm:'+''+ Delivery +'\n' +
+    'Qanday:'+''+ Vocation +'\n' +
+    'Osh:'+''+ Size +'\n' +
+    'Baxo:'+''+ Numbern +'\n' +
+    'Hobby:'+''+ Occupation +'\n' +
+    'Davlatlar:'+''+ Type +'\n' +
+    'Men:'+''+ info +'\n'
+    ]
+
+    alert(All)
